8658205fa883d780dc9bc1337fc26c998c8160d3
AngusJPurcell <purciea242@aol.co.uk>
2024-02-02 13:54:34 +0000

    Update lumigoo.ino

--- lumigoo/lumigoo.ino
+++ lumigoo/lumigoo.ino
@@ -1,3 +1,27 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_CAP1188.h>
+
+// Reset Pin is used for I2C or SPI
+#define CAP1188_RESET  9
+
+// CS pin is used for software or hardware SPI
+#define CAP1188_CS  10
+
+// These are defined for software SPI, for hardware SPI, check your 
+// board's SPI pins in the Arduino documentation
+#define CAP1188_MOSI  11
+#define CAP1188_MISO  12
+#define CAP1188_CLK  13
+
+// For I2C, connect SDA to your Arduino's SDA pin, SCL to SCL pin
+// On UNO/Duemilanove/etc, SDA == Analog 4, SCL == Analog 5
+// On Leonardo/Micro, SDA == Digital 2, SCL == Digital 3
+// On Mega/ADK/Due, SDA == Digital 20, SCL == Digital 21
+
+// Use I2C, no reset pin!
+Adafruit_CAP1188 cap = Adafruit_CAP1188();
+
 const int buttonPin = 2;
 const int motorPin = 6;
 
@@ -11,6 +35,7 @@ int motorState = 0;
 bool down = false;
 int vibrateOn = 0;
 
+
 int overfeed(int food) {
   if(food > 60){
       food = 60;
@@ -23,6 +48,17 @@ int overfeed(int food) {
 
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Serial.println("CAP1188 test!");
+
+  // Initialize the sensor, if using i2c you can pass in the i2c address
+  // if (!cap.begin(0x28)) {
+  if (!cap.begin()) {
+    Serial.println("CAP1188 not found");
+    while (1);
+  }
+  Serial.println("CAP1188 found!");
 
   pinMode(buttonPin, INPUT);
   pinMode(motorPin, OUTPUT);
@@ -70,5 +106,20 @@ void loop() {
   } else if (buttonState == LOW) {
     down = false;
   }
+
+  uint8_t touched = cap.touched();
+
+  if (touched == 0) {
+    // No touch detected
+    return;
+  }
+  
+  for (uint8_t i=0; i<8; i++) {
+    if (touched & (1 << i)) {
+      Serial.print("C"); Serial.print(i+1); Serial.print("\t");
+    }
+  }
+  Serial.println();
+  delay(50);
 }
 
